d84997594a924c5d49425ba0981cf23f1ae7bdd0
xand974 <xand974@gmail.com>
2021-10-01 12:14:45 +0200

    front : fix bug + pop up

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import MapBoxGL, { Marker, Popup } from "react-map-gl";
 import styled from "styled-components";
-import { PushPin, StarOutlined } from "@mui/icons-material";
+import { RoomOutlined, StarOutlined } from "@mui/icons-material";
 import { useEffect } from "react";
 import api from "config/api";
 
@@ -10,7 +10,7 @@ const Container = styled.div`
   width: 100%;
 `;
 const PopUpContainer = styled.div`
-  width: 300px;
+  width: 250px;
   height: 300px;
   background-color: #ffffff5b;
   display: flex;
@@ -22,36 +22,42 @@ const Title = styled.h1`
   text-align: center;
   width: 100%;
   color: #1a2637;
+  &:first-letter {
+    text-transform: capitalize;
+  }
 `;
 
 const MiniCard = styled.div`
-  width: 100px;
-  height: 100px;
-  margin: 10px;
-  box-shadow: 0 0 10px #0000005b;
+  margin: 10px 20px;
   border-radius: 5px;
   padding: 10px;
   display: flex;
-  flex-direction: column;
+  align-items: center;
   justify-content: space-between;
 `;
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  padding: 20px;
-`;
 const Label = styled.label`
   color: #1a2637;
   font-size: 15px;
   font-weight: bold;
+  border-bottom: 1px solid #1a2637;
 `;
 
 const Info = styled.p`
   color: gray;
-  font-size: 12px;
+  font-size: 13px;
+  display: flex;
+  align-items: center;
 `;
+
+const Description = styled.span`
+  color: gray;
+  font-size: 13px;
+  margin-left: 20px;
+`;
+
 export default function Home() {
+  const [pinSelected, setPinSelected] = useState(null);
   const [pins, setPins] = useState([]);
   const [viewport, setViewport] = useState({
     width: "100vw",
@@ -60,11 +66,10 @@ export default function Home() {
     longitude: 3.876716,
     zoom: 8,
   });
-
   useEffect(() => {
     const fetchPins = async () => {
       try {
-        const res = api.get("/pins/all", {
+        const res = await api.get("/pins/all", {
           headers: {
             token:
               "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTU0YjQ2MDIxZDYwNmI0N2Y5MDkyZjQiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2MzMwMzE3NTYsImV4cCI6MTYzMzExODE1Nn0.eA7IJsqukwo7domVdN8p2FYMHt4Fot1AcrllxgYOLLU",
@@ -86,57 +91,71 @@ export default function Home() {
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         mapStyle="mapbox://styles/xand974/cku5ipsl12agz18l6u2rvopzc"
       >
-        {pins.map((pin) => {
+        {pins.map((pin, key) => {
           return (
             <>
               <Marker
+                key={pin._id}
                 longitude={pin.longitude}
                 latitude={pin.latitude}
                 offsetLeft={-20}
                 offsetTop={-10}
+                onClick={() => {
+                  setPinSelected(pin._id);
+                }}
               >
-                <PushPin
-                  style={{ color: "crimson", fontSize: viewport.zoom * 3 }}
+                <RoomOutlined
+                  style={{
+                    color: "crimson",
+                    fontSize: viewport.zoom * 10,
+                    cursor: "pointer",
+                  }}
                 />
               </Marker>
-              <Popup
-                longitude={3}
-                latitude={43}
-                anchor="left"
-                closeButton={true}
-                closeOnClick={false}
-              >
-                <PopUpContainer>
-                  <Title>{pin.title}</Title>
-                  <Wrapper>
-                    <MiniCard>
-                      <Label>Description</Label>
-                      <Info>Un endroit assez spécial où vit du monde</Info>
-                    </MiniCard>
+              {pinSelected === pin._id && (
+                <Popup
+                  tipSize="20px"
+                  key={key}
+                  longitude={pin.longitude}
+                  latitude={pin.latitude}
+                  anchor="right"
+                  closeButton={true}
+                  closeOnClick={false}
+                  onClose={() => setPinSelected(null)}
+                >
+                  <PopUpContainer>
+                    <Title>{pin.title}</Title>
+
                     <MiniCard>
                       <Label>Created By</Label>
-                      <Info>{pin.username}</Info>
+                      <Info style={{ color: "crimson" }}>{pin.username}</Info>
+                    </MiniCard>
+                    <MiniCard>
+                      <Label>Description</Label>
+                      <Description>
+                        {pin.description?.length > 23
+                          ? pin.description?.substring(0, 23) + "..."
+                          : pin.description || "endroit assez spécial ou.."}
+                      </Description>
                     </MiniCard>
                     <MiniCard>
                       <Label>Rating</Label>
                       <Info>
-                        <StarOutlined />
-                        <StarOutlined />
-                        <StarOutlined />
-                        <StarOutlined />
-                        <StarOutlined />
+                        {pin.rating}
+                        <StarOutlined style={{ color: "crimson" }} />
                       </Info>
                     </MiniCard>
                     <MiniCard>
                       <Label>When ?</Label>
-                      <Info>{pin.createdAt}</Info>
+                      <Info>{pin.createdAt || "10 mins ago"}</Info>
                     </MiniCard>
-                  </Wrapper>
-                </PopUpContainer>
-              </Popup>
+                  </PopUpContainer>
+                </Popup>
+              )}
             </>
           );
         })}
+        );
       </MapBoxGL>
     </Container>
   );
